78df336d458c5de3dd61affad04b303156736a06
Bruno Gomes de Souza <124291749+brunogsouza10@users.noreply.github.com>
2023-10-01 22:41:23 +0000

    Controle Remoto

---
+++ aula001_POO/Interface/interface.java
@@ -0,0 +1,25 @@
+package encapsulamento;
+
+public interface Controlador {
+	
+	//MÉTODOS ABSTRATOS - INTERFACE
+	
+	public abstract void ligar(); 
+	public abstract void desligar();
+	public abstract void canalMais();
+	public abstract void canalMenos();
+	public abstract void apertarBotao();
+	public abstract void abrirMenu();
+	public abstract void fecharMenu();
+	public abstract void maisVolume();
+	public abstract void menosVolume();
+	public abstract void ligarMudo();
+	public abstract void desligarMudo();
+	public abstract void usarHdmi();
+	public abstract void play();
+	public abstract void rec();
+	public abstract void pause();
+	
+	
+	
+}
