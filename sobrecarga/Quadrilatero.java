1fdaa595966591abc26bfc02d309678b835b31bf
Bruno Gomes de Souza <124291749+brunogsouza10@users.noreply.github.com>
2023-10-30 23:46:06 +0000

    exemplo de sobrecarga

--- sobrecarga/Quadrilatero.java
+++ sobrecarga/Quadrilatero.java
@@ -1,5 +1,19 @@
 package sobrecarga;
 
 public class Quadrilatero {
+	
+	public double calcularArea( double lado) {
+	return lado * lado;
+	}
+	public double calcularArea(double baseMaior, double baseMenor) {
+	return baseMaior * baseMenor;	
+	}
+	public double calcularArea(double baseMaior, double baseMenor, double altura) {
+	return ((baseMaior + baseMenor) * altura)/2;	
+	}
+	public double calcularArea(float diagonalMaior, float diagonalMenor) {
+	return diagonalMaior * diagonalMenor;	
+		
+	}
 
 }
